36f7a0a89da95e86fb95dce4602185f917fc5124
quoderatdemons <taminr1902@gmail.com>
2026-09-08 16:10:02 -0400

    Revert "ugh"
    This reverts commit 162a7ad9c4a2fa907ff460b3ecb37f43f788ba06.

--- src/commands.js
+++ src/commands.js
@@ -40,4 +40,4 @@ const INVENTORY_COMMAND = {
 
 const ALL_COMMANDS = [FISH_COMMAND, REGISTER_COMMAND, INVENTORY_COMMAND];
 
-InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
+InstallGlobalCommands(env.APP_ID, ALL_COMMANDS);
